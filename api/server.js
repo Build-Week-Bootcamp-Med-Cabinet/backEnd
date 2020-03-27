ace386062572eeed86715ef9a45864a2b8e4d958
chirag.thesia <harithesia@gmail.com>
2020-03-26 21:51:59 -0500

    SERVER UPDATED

--- api/server.js
+++ api/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const server = express();
+
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const axios = require('axios');
 
 const authenticate = require('../auth/auth-middleware');
 const authRouter = require('../auth/auth-router');
 const userRouter = require('../users/userRouter');
-
-const server = express();
 
 server.use(helmet());
 server.use(cors());
@@ -22,7 +21,10 @@ server.use('/api/user', userRouter);
 
 server.use('/', (req, res, next) => {
 	try {
-		res.status(200).json({ message: 'Welcome to the API. Please find all API docs online in the GITHUB Repo!' });
+		res.status(200).json({
+			message:
+				'Welcome to the API. Please find all API docs online in the GITHUB Repo! https://github.com/Build-Week-Bootcamp-Med-Cabinet/backEnd'
+		});
 	} catch (err) {
 		next(err);
 	}
