e48a5f59ab271a8f74291c740d09216488f105c0
chirag.thesia <harithesia@gmail.com>
2020-03-26 20:31:52 -0500

    Created user CRUD points. Login works

--- database/migrations/20200324190647_med_cabinate.js
+++ database/migrations/20200324190647_med_cabinate.js
@@ -1,8 +1,45 @@
-
 exports.up = function(knex) {
-  
+	return (
+		knex.schema
+			.createTable('users', (table) => {
+				table.increments();
+				table.string('firstname').notNullable();
+				table.string('lastName').notNullable();
+				table.string('email').notNullable().unique();
+				table.string('username').notNullable().unique();
+				table.string('password').notNullable();
+			})
+			//Strain Table
+			.createTable('strains', (table) => {
+				table.increments();
+				table.string('name').notNullable();
+			})
+			//Join table
+			.createTable('user-strains', (table) => {
+				table
+					.integer('user_id')
+					.notNullable()
+					.references('id')
+					.inTable('users')
+					.onDelete('CASCADE')
+					.onUpdate('CASCADE');
+
+				table
+					.integer('strain_id')
+					.notNullable()
+					.references('id')
+					.inTable('strains')
+					.onDelete('CASCADE')
+					.onUpdate('CASCADE');
+
+				table.primary([ 'user_id', 'strain_id' ]);
+			})
+	);
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists('user-strains');
+
+	await knex.schema.dropTableIfExists('strains');
+	await knex.schema.dropTableIfExists('users');
 };
